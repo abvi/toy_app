2ae7b524fffc4c45cc15bc1fc43f1c8fe43e644a
Abhijeet Vijayakar <abhijeet.vijayakar@gmail.com>
2015-07-30 00:28:50 -0700

    Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  
-  def index
-    @title = "Welcome to Abhi's Users App!";
-    @body = "View Users";
-    @link = "/users";
-  end
+  protect_from_forgery
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = '90a56f56aa547dbb1920d4230955fdeb61c610052e384a1e1c27624b839c3b1f2197e6fb8eb2fe5140b0922fca878e3e085f86db7f8db5e5b31ef50d0482d7a0'
